0f029cd9edbbd03a6b24a92129432e131fc51a1d
tuorqai <tuorqai@gmail.com>
2024-07-19 20:43:11 +0900

    Fri, 19 Jul, 2024 11:43:11

--- secamiz0r.c
+++ secamiz0r.c
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2023 tuorqai
+ * Copyright (c) 2024 tuorqai
  * 
  * This software is provided 'as-is', without any express or implied
  * warranty. In no event will the authors be held liable for any damages
